f25acf57b9f7fe3b8ac316572be42c3709c09d24
Justin Cox <Justinsterlingcc@gmail.com>
2021-07-24 20:51:47 -0400

    wip calc pre-buttons/code

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Calculator
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.bttn_plus = new System.Windows.Forms.Button();
@@ -53,6 +54,7 @@ namespace Calculator
             this.bttn_clear = new System.Windows.Forms.Button();
             this.bttn_equals = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
@@ -76,14 +78,23 @@ namespace Calculator
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(448, 437);
-            this.splitContainer1.SplitterDistance = 88;
+            this.splitContainer1.Size = new System.Drawing.Size(316, 357);
+            this.splitContainer1.SplitterDistance = 72;
             this.splitContainer1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Elephant", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 90);
+            this.textBox1.TabIndex = 0;
             // 
             // splitContainer2
             // 
@@ -98,8 +109,8 @@ namespace Calculator
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
-            this.splitContainer2.Size = new System.Drawing.Size(448, 345);
-            this.splitContainer2.SplitterDistance = 232;
+            this.splitContainer2.Size = new System.Drawing.Size(316, 281);
+            this.splitContainer2.SplitterDistance = 163;
             this.splitContainer2.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -132,15 +143,15 @@ namespace Calculator
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(232, 345);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(163, 281);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // bttn_plus
             // 
             this.bttn_plus.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_plus.Location = new System.Drawing.Point(3, 279);
+            this.bttn_plus.Location = new System.Drawing.Point(3, 227);
             this.bttn_plus.Name = "bttn_plus";
-            this.bttn_plus.Size = new System.Drawing.Size(71, 63);
+            this.bttn_plus.Size = new System.Drawing.Size(48, 51);
             this.bttn_plus.TabIndex = 28;
             this.bttn_plus.Text = "+";
             this.bttn_plus.UseVisualStyleBackColor = true;
@@ -148,9 +159,9 @@ namespace Calculator
             // bttn_3
             // 
             this.bttn_3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_3.Location = new System.Drawing.Point(157, 210);
+            this.bttn_3.Location = new System.Drawing.Point(111, 171);
             this.bttn_3.Name = "bttn_3";
-            this.bttn_3.Size = new System.Drawing.Size(72, 63);
+            this.bttn_3.Size = new System.Drawing.Size(49, 50);
             this.bttn_3.TabIndex = 27;
             this.bttn_3.Text = "3";
             this.bttn_3.UseVisualStyleBackColor = true;
@@ -158,9 +169,9 @@ namespace Calculator
             // bttn_2
             // 
             this.bttn_2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_2.Location = new System.Drawing.Point(80, 210);
+            this.bttn_2.Location = new System.Drawing.Point(57, 171);
             this.bttn_2.Name = "bttn_2";
-            this.bttn_2.Size = new System.Drawing.Size(71, 63);
+            this.bttn_2.Size = new System.Drawing.Size(48, 50);
             this.bttn_2.TabIndex = 26;
             this.bttn_2.Text = "2";
             this.bttn_2.UseVisualStyleBackColor = true;
@@ -168,20 +179,19 @@ namespace Calculator
             // bttn_1
             // 
             this.bttn_1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_1.Location = new System.Drawing.Point(3, 210);
+            this.bttn_1.Location = new System.Drawing.Point(3, 171);
             this.bttn_1.Name = "bttn_1";
-            this.bttn_1.Size = new System.Drawing.Size(71, 63);
+            this.bttn_1.Size = new System.Drawing.Size(48, 50);
             this.bttn_1.TabIndex = 25;
             this.bttn_1.Text = "1";
             this.bttn_1.UseVisualStyleBackColor = true;
-            this.bttn_1.Click += new System.EventHandler(this.button10_Click);
             // 
             // bttn_6
             // 
             this.bttn_6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_6.Location = new System.Drawing.Point(157, 141);
+            this.bttn_6.Location = new System.Drawing.Point(111, 115);
             this.bttn_6.Name = "bttn_6";
-            this.bttn_6.Size = new System.Drawing.Size(72, 63);
+            this.bttn_6.Size = new System.Drawing.Size(49, 50);
             this.bttn_6.TabIndex = 24;
             this.bttn_6.Text = "6";
             this.bttn_6.UseVisualStyleBackColor = true;
@@ -189,9 +199,9 @@ namespace Calculator
             // bttn_5
             // 
             this.bttn_5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_5.Location = new System.Drawing.Point(80, 141);
+            this.bttn_5.Location = new System.Drawing.Point(57, 115);
             this.bttn_5.Name = "bttn_5";
-            this.bttn_5.Size = new System.Drawing.Size(71, 63);
+            this.bttn_5.Size = new System.Drawing.Size(48, 50);
             this.bttn_5.TabIndex = 23;
             this.bttn_5.Text = "5";
             this.bttn_5.UseVisualStyleBackColor = true;
@@ -199,9 +209,9 @@ namespace Calculator
             // bttn_4
             // 
             this.bttn_4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_4.Location = new System.Drawing.Point(3, 141);
+            this.bttn_4.Location = new System.Drawing.Point(3, 115);
             this.bttn_4.Name = "bttn_4";
-            this.bttn_4.Size = new System.Drawing.Size(71, 63);
+            this.bttn_4.Size = new System.Drawing.Size(48, 50);
             this.bttn_4.TabIndex = 22;
             this.bttn_4.Text = "4";
             this.bttn_4.UseVisualStyleBackColor = true;
@@ -209,9 +219,9 @@ namespace Calculator
             // bttn_9
             // 
             this.bttn_9.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_9.Location = new System.Drawing.Point(157, 72);
+            this.bttn_9.Location = new System.Drawing.Point(111, 59);
             this.bttn_9.Name = "bttn_9";
-            this.bttn_9.Size = new System.Drawing.Size(72, 63);
+            this.bttn_9.Size = new System.Drawing.Size(49, 50);
             this.bttn_9.TabIndex = 21;
             this.bttn_9.Text = "9";
             this.bttn_9.UseVisualStyleBackColor = true;
@@ -219,9 +229,9 @@ namespace Calculator
             // bttn_8
             // 
             this.bttn_8.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_8.Location = new System.Drawing.Point(80, 72);
+            this.bttn_8.Location = new System.Drawing.Point(57, 59);
             this.bttn_8.Name = "bttn_8";
-            this.bttn_8.Size = new System.Drawing.Size(71, 63);
+            this.bttn_8.Size = new System.Drawing.Size(48, 50);
             this.bttn_8.TabIndex = 20;
             this.bttn_8.Text = "8";
             this.bttn_8.UseVisualStyleBackColor = true;
@@ -229,9 +239,9 @@ namespace Calculator
             // bttn_7
             // 
             this.bttn_7.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_7.Location = new System.Drawing.Point(3, 72);
+            this.bttn_7.Location = new System.Drawing.Point(3, 59);
             this.bttn_7.Name = "bttn_7";
-            this.bttn_7.Size = new System.Drawing.Size(71, 63);
+            this.bttn_7.Size = new System.Drawing.Size(48, 50);
             this.bttn_7.TabIndex = 19;
             this.bttn_7.Text = "7";
             this.bttn_7.UseVisualStyleBackColor = true;
@@ -239,9 +249,9 @@ namespace Calculator
             // bttn_decimal
             // 
             this.bttn_decimal.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_decimal.Location = new System.Drawing.Point(157, 3);
+            this.bttn_decimal.Location = new System.Drawing.Point(111, 3);
             this.bttn_decimal.Name = "bttn_decimal";
-            this.bttn_decimal.Size = new System.Drawing.Size(72, 63);
+            this.bttn_decimal.Size = new System.Drawing.Size(49, 50);
             this.bttn_decimal.TabIndex = 18;
             this.bttn_decimal.Text = ".";
             this.bttn_decimal.UseVisualStyleBackColor = true;
@@ -249,9 +259,9 @@ namespace Calculator
             // bttn_division
             // 
             this.bttn_division.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_division.Location = new System.Drawing.Point(80, 3);
+            this.bttn_division.Location = new System.Drawing.Point(57, 3);
             this.bttn_division.Name = "bttn_division";
-            this.bttn_division.Size = new System.Drawing.Size(71, 63);
+            this.bttn_division.Size = new System.Drawing.Size(48, 50);
             this.bttn_division.TabIndex = 17;
             this.bttn_division.Text = "/";
             this.bttn_division.UseVisualStyleBackColor = true;
@@ -261,18 +271,17 @@ namespace Calculator
             this.bttn_x.Dock = System.Windows.Forms.DockStyle.Fill;
             this.bttn_x.Location = new System.Drawing.Point(3, 3);
             this.bttn_x.Name = "bttn_x";
-            this.bttn_x.Size = new System.Drawing.Size(71, 63);
+            this.bttn_x.Size = new System.Drawing.Size(48, 50);
             this.bttn_x.TabIndex = 16;
             this.bttn_x.Text = "X";
             this.bttn_x.UseVisualStyleBackColor = true;
-            this.bttn_x.Click += new System.EventHandler(this.button1_Click);
             // 
             // bttn_0
             // 
             this.bttn_0.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_0.Location = new System.Drawing.Point(80, 279);
+            this.bttn_0.Location = new System.Drawing.Point(57, 227);
             this.bttn_0.Name = "bttn_0";
-            this.bttn_0.Size = new System.Drawing.Size(71, 63);
+            this.bttn_0.Size = new System.Drawing.Size(48, 51);
             this.bttn_0.TabIndex = 15;
             this.bttn_0.Text = "0";
             this.bttn_0.UseVisualStyleBackColor = true;
@@ -280,13 +289,12 @@ namespace Calculator
             // bttn_minus
             // 
             this.bttn_minus.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_minus.Location = new System.Drawing.Point(157, 279);
+            this.bttn_minus.Location = new System.Drawing.Point(111, 227);
             this.bttn_minus.Name = "bttn_minus";
-            this.bttn_minus.Size = new System.Drawing.Size(72, 63);
+            this.bttn_minus.Size = new System.Drawing.Size(49, 51);
             this.bttn_minus.TabIndex = 14;
             this.bttn_minus.Text = "-";
             this.bttn_minus.UseVisualStyleBackColor = true;
-            this.bttn_minus.Click += new System.EventHandler(this.button15_Click);
             // 
             // splitContainer3
             // 
@@ -297,9 +305,8 @@ namespace Calculator
             // splitContainer3.Panel1
             // 
             this.splitContainer3.Panel1.Controls.Add(this.tableLayoutPanel2);
-            this.splitContainer3.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer3_Panel1_Paint);
-            this.splitContainer3.Size = new System.Drawing.Size(212, 345);
-            this.splitContainer3.SplitterDistance = 85;
+            this.splitContainer3.Size = new System.Drawing.Size(149, 281);
+            this.splitContainer3.SplitterDistance = 59;
             this.splitContainer3.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -314,7 +321,7 @@ namespace Calculator
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.14493F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 69.85507F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(85, 345);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(59, 281);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // bttn_clear
@@ -322,7 +329,7 @@ namespace Calculator
             this.bttn_clear.Dock = System.Windows.Forms.DockStyle.Fill;
             this.bttn_clear.Location = new System.Drawing.Point(3, 3);
             this.bttn_clear.Name = "bttn_clear";
-            this.bttn_clear.Size = new System.Drawing.Size(79, 98);
+            this.bttn_clear.Size = new System.Drawing.Size(53, 78);
             this.bttn_clear.TabIndex = 0;
             this.bttn_clear.Text = "C";
             this.bttn_clear.UseVisualStyleBackColor = true;
@@ -330,9 +337,9 @@ namespace Calculator
             // bttn_equals
             // 
             this.bttn_equals.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bttn_equals.Location = new System.Drawing.Point(3, 107);
+            this.bttn_equals.Location = new System.Drawing.Point(3, 87);
             this.bttn_equals.Name = "bttn_equals";
-            this.bttn_equals.Size = new System.Drawing.Size(79, 235);
+            this.bttn_equals.Size = new System.Drawing.Size(53, 191);
             this.bttn_equals.TabIndex = 1;
             this.bttn_equals.Text = "=";
             this.bttn_equals.UseVisualStyleBackColor = true;
@@ -341,10 +348,12 @@ namespace Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 437);
+            this.ClientSize = new System.Drawing.Size(316, 357);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
@@ -385,6 +394,7 @@ namespace Calculator
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.Button bttn_clear;
         private System.Windows.Forms.Button bttn_equals;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
